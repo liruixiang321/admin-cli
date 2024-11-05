47603d3302f62c14f956b84a43d285b2ce12bb04
liruixaing <3054809485@qq.com>
2024-11-05 22:54:22 +0800

    ci: 初始化代码提交和风格检查配置
    - 添加 commitlint 配置文件，规范代码提交信息
    - 新增 ESLint、Prettier、Stylelint 配置文件，统一代码风格
    - 在 package.json 中添加相关脚本和依赖，支持代码检查和格式化
    - 忽略特定文件和目录的检查，提高开发效率

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,5 @@ import App from "./App.vue";
 import "./styles/reset.css";
 import router from "./router";
 import pinia from "./store";
-const app = createApp(App).use(router).use(pinia).mount("#app");
+const app = createApp(App)
+app.use(router).use(pinia).mount("#app");
