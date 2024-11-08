4086db9af341ddf0fcf07af273330fd87e88f83e
liruixaing <3054809485@qq.com>
2024-11-08 15:51:55 +0800

    feat(user): 添加用户列表功能
    - 在 mock 文件中添加了获取用户列表的接口
    - 在 api 文件中添加了 getUserList 方法
    - 在 views 文件中添加了用户列表的展示页面
    - 更新了 types 文件，添加了新的组件声明

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,4 @@
-import { post } from '@/http/request';
+import { get, post } from '@/http/request';
 
 export type loginRequest = {
   username: string;
@@ -12,10 +12,23 @@ export type loginResponse = {
   username: string;
   roles: [];
 };
+export type userItem = {
+  id: number;
+  nickname: string;
+  username: string;
+  roles: Array<{
+    role: number;
+    roleName: string;
+  }>;
+};
 export const userLogin = async (data?: loginRequest) => {
   return await post<loginResponse>({}, '/login', data);
 };
 
 export const refreshUserInfo = async (data?: reLoginRequest) => {
   return await post<loginResponse>({}, '/refreshLogin', data);
+};
+
+export const getUserList = async () => {
+  return await get<Array<userItem>>({}, '/getUserList');
 };
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,3 +1,47 @@
 <template>
-  <div> 用户列表 </div>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column label="ID" width="180">
+      <template #default="scope">
+        <div style="display: flex; align-items: center">
+          <span style="margin-left: 10px">{{ scope.row.id }}</span>
+        </div>
+      </template>
+    </el-table-column>
+    <el-table-column label="NickName" width="180">
+      <template #default="scope">
+        {{ scope.row.nickName }}
+      </template>
+    </el-table-column>
+    <el-table-column label="roles" width="180">
+      <template #default="scope">
+        <div class="flex gap-2">
+          <el-tag v-for="item in scope.row.roles" :key="item.role">{{ item.roleName }}</el-tag>
+        </div>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="Operations">
+      <template #default="scope">
+        <el-button size="small" @click="handleEdit(scope.$index, scope.row)"> Edit </el-button>
+        <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">
+          Delete
+        </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
+
+<script lang="ts" setup>
+  import { getUserList, userItem } from '@/api/user';
+  import { onMounted, ref } from 'vue';
+
+  const tableData = ref<userItem[]>([]);
+  const fetchUserList = () => {
+    return getUserList().then((res) => {
+      tableData.value = res;
+    });
+  };
+  onMounted(() => {
+    fetchUserList();
+  });
+</script>
